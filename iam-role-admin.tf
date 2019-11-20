31d92d76a455802eb82a816b21f3b93b5aa8f752
Allan Denot <adenot@gmail.com>
2019-11-20 17:31:44 +1100

    Moving policy to inline json to avoid problem when using count with data

--- iam-role-admin.tf
+++ iam-role-admin.tf
@@ -1,35 +1,33 @@
-data "aws_iam_policy_document" "assume_role_admin" {
-  count = "${var.idp_account_id != "" ? 1 : 0}"
-
-  statement = {
-    principals = {
-      type = "AWS"
-
-      identifiers = ["${concat(
-        list(
-          "arn:aws:iam::${var.idp_account_id}:root"
-        ), 
-          formatlist(
-            "arn:aws:iam::%s:role/${var.org_name}-admin", var.idp_admin_trust_account_ids
-          ),
-          formatlist(
-            "arn:aws:iam::%s:role/client-admin", var.idp_admin_trust_account_ids
-          )
-        )
-      }"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-  }
-}
-
 resource "aws_iam_role" "admin" {
   count = "${var.idp_account_id != "" ? 1 : 0}"
 
   name                 = "${var.org_name}-${var.account_name}-admin"
-  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_admin.json}"
+  assume_role_policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS":
+          ${jsonencode(concat(
+            list(
+              "arn:aws:iam::${var.idp_account_id}:root"
+            ), 
+            formatlist(
+              "arn:aws:iam::%s:role/${var.org_name}-admin", var.idp_admin_trust_account_ids
+            ),
+            formatlist(
+              "arn:aws:iam::%s:role/client-admin", var.idp_admin_trust_account_ids
+            )
+          ))}
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
   max_session_duration = "${var.role_max_session_duration}"
 }
 
--- iam-role-read-only.tf
+++ iam-role-read-only.tf
@@ -1,35 +1,33 @@
-data "aws_iam_policy_document" "assume_role_read_only" {
-  count = "${var.idp_account_id != "" ? 1 : 0}"
-
-  statement = {
-    principals = {
-      type = "AWS"
-
-      identifiers = ["${concat(
-        list(
-          "arn:aws:iam::${var.idp_account_id}:root",
-        ), 
-          formatlist(
-            "arn:aws:iam::%s:role/${var.org_name}-read-only", var.idp_admin_trust_account_ids
-          ),
-          formatlist(
-            "arn:aws:iam::%s:role/client-read-only", var.idp_admin_trust_account_ids
-          )
-        )
-      }"]
-    }
-
-    actions = [
-      "sts:AssumeRole",
-    ]
-  }
-}
-
 resource "aws_iam_role" "read_only" {
   count = "${var.idp_account_id != "" ? 1 : 0}"
 
   name                 = "${var.org_name}-${var.account_name}-read-only"
-  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_read_only.json}"
+  assume_role_policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS":
+          ${jsonencode(concat(
+            list(
+              "arn:aws:iam::${var.idp_account_id}:root"
+            ), 
+            formatlist(
+              "arn:aws:iam::%s:role/${var.org_name}-read-only", var.idp_admin_trust_account_ids
+            ),
+            formatlist(
+              "arn:aws:iam::%s:role/client-read-only", var.idp_admin_trust_account_ids
+            )
+          ))}
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
   max_session_duration = "${var.role_max_session_duration}"
 }
 
